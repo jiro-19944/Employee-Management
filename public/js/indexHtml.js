f8de3d7f697b5bd51e6e8528e4c883f746dfae15
Jirayr Stepanyan <jiro-19944@mail.com>
2016-11-21 22:55:56 +0400

    Updated header.css, index.html and indexHtml.js files and added main.css and plus.jpg files

--- public/js/indexHtml.js
+++ public/js/indexHtml.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("#1st_li, #2rd_li, #3nd_li, #4th_li, #5th_li").click(function(){
+    $("#employee, #leave, #salary, #footballTeam").click(function(){
     	$("#header_ul li").css('background-color', '#4C87BF');
     	$("#header_ul li a").css('color', 'white');
         $(this).css('background-color', 'white');
