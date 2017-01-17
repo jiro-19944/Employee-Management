7a97c7b1120753e7fe62421531b38dfe7c6faeb4
Jirayr <jiro-19944@mail.ru>
2017-01-18 02:27:18 +0400

    - Added new uls and lis for personal info

--- public/js/indexHtml.js
+++ public/js/indexHtml.js
@@ -5,11 +5,4 @@ $(document).ready(function(){
         $(this).css('background-color', 'white');
         $(this).find("a").css('color', '#4C87BF');
     });
-
-    $("#basic_info_li, #personal_li, #job_li, #salary_li, #vocation_li").click(function(){
-    	$("#basic_info_ul li").css('background-color', 'white');
-    	$("#basic_info_ul li a").css('color', '#4172AA');
-        $(this).css('background-color', '#B8D1E7');
-        $(this).find("a").css('color', '#005D8A');
-    });
 });
