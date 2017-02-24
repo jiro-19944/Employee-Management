d4437589c9a3aef5e9beef396dfabb7a50b3c010
Jirayr <jiro-19944@mail.ru>
2017-02-24 23:48:24 +0400

    - Added sign up and sign in functionality wich works with mongo db

--- controller.js
+++ controller.js
@@ -1,36 +1,74 @@
-var a = {};
+var db = require("./db");
+var myDb;
+var isDbConnected = false;
+
+var checkAndConnectToDb = function()
+{
+	if(!isDbConnected)
+	{
+		isDbConnected = db.connectToDB(isDbConnected);
+	}
+	else
+	{
+		console.log("------------ DB is already conected -------------");
+	}
+}
+
+var checkAndGetDb = function()
+{
+	if(!myDb)
+	{
+		myDb = db.getDB();
+	}
+	else
+	{
+		console.log("------------ DB is already got -------------");
+	}
+}
 
 module.exports.home = function(req, res)
 {
-	// var body = req.body
-	// console.log("stacvec ------------  ");
-	// console.log(body);
 	res.render('./html/home.html');
+	checkAndConnectToDb();
 };
 
 module.exports.signIn = function(req, res)
 {
-	var body = req.body
-	console.log("------------ post ------------  sign IN ----------------");
-	if(body.Email === a.Email && body.Password === a.Password)
+	checkAndConnectToDb();
+	checkAndGetDb();
+	var body = req.body;
+	myDb.collection('userPass', function(err, collection)
 	{
-		console.log("/////////////////////////////////  " + a.Email);
-		res.render('./html/signedIn.html');
-	}
-	else
-	{
-		console.log("------------  invalid email or password ----------------");
-	}
-	// res.render('./html/index.html');
-	// res.end("++++++++++++++++++++++++ " + body.Password);
+		collection.findOne({email: body.email}, function(err, item)
+		{
+			if(!item)
+			{
+				console.log("Wrong username: Please sign up");
+			}
+			else
+			{
+				if(body.password == item.password)
+				{
+					res.render('./html/signedIn.html');
+				}
+				else
+				{
+					console.log("The password is wrong");
+				}
+			}
+		});
+	});
+	
 };
 
 module.exports.signUp = function(req, res)
 {
-	var body = req.body
-	console.log("------------ post ------------ sign UP -----------  ");
-	a.Email = body.Email;
-	a.Password = body.Password;
-	res.render('./html/home.html');
-	console.log("++++++++++++++++++++++++ greciiiiiiiiiiiiiii" + a.Email);
+	checkAndConnectToDb();
+	checkAndGetDb();
+	var body = req.body;
+	myDb.createCollection('userPass', function(err, collection)
+    {
+    	collection.insert(body);
+    	console.log("++++++++++++++++++++++++ greciiiiiiiiiiiiiii");
+    });
 };
---
+++ db.js
@@ -0,0 +1,21 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/employeeManagement";
+var mongoDB = {};
+
+module.exports = {
+	connectToDB: function() {
+		MongoClient.connect(url, function(err, db)
+		{
+		    if(err)
+		    {
+		    	return console.dir(err);
+		    }
+			mongoDB = db;
+	    });
+		return true;
+	},
+
+	getDB: function() {
+		return mongoDB;
+	}
+};
