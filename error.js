f796d93d58d8d5bc04d7221e9ef1c62e6e437670
Jirayr <jiro-19944@mail.ru>
2017-07-06 21:59:56 +0400

    Added crypto module and made some updates to work with mongoose

--- error.js
+++ error.js
@@ -1,11 +1,24 @@
-module.exports.databaseError = function(dbName) {
-	console.log('database connecting error');
+module.exports.databaseError = function() {
+	console.log('database error');
+	return {
+        errorCode: '500',
+        errorMessage: 'Internal Server Error',
+        message: 'Our service team is working to bring it back online.'
+    };
 };
 
 module.exports.collectionError = function() {
 	console.log('database collection error');
-}
+};
 
 module.exports.callbackError = function() {
 	console.log('There is an issue related to nodejs callback function');
+};
+
+module.exports.pageNotFound = function() {
+    return {
+        errorCode: '404',
+        errorMessage: 'Page Not Found',
+        message: 'The requested resource could not be found.'
+    };
 }
--- user.js
+++ user.js
@@ -1,20 +1,19 @@
 var crypto = require('crypto'),
 	dbSchemas = require('./dbSchemas'),
-    mongoose = require('./db'),
-    schema = mongoose.Schema(dbSchemas.user);
+	mongoose = require('./db'),
+	schema = mongoose.Schema(dbSchemas.user);
 
 schema.statics.encryptPassword = function (password) {
-    var algorithm = 'aes-256-ctr',
+	var algorithm = 'aes-256-ctr',
 		key = '5gtLK87pOd',
 		cipher = crypto.createCipher(algorithm,key),
-    	cryptedPassword = cipher.update(password,'utf8','hex');
-    cryptedPassword += cipher.final('hex');
-    return cryptedPassword;
+		cryptedPassword = cipher.update(password,'utf8','hex');
+	cryptedPassword += cipher.final('hex');
+	return cryptedPassword;
 };
 
-schema.statics.checkPassword = function (password, user) {
-	console.log('++++++++++++  ', this.encryptPassword(password), '  ++++++++  ', user.password);
-	return this.encryptPassword(password) === user.password;
+schema.statics.checkPassword = function (password, correctPassowrd) {
+	return this.encryptPassword(password) === correctPassowrd;
 };
 
 module.exports.User = mongoose.model('User', schema);
